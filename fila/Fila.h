bb1858ad6d74171d3bf1a755fdb86da8b7e2bbb2
Leonardo Anjos <leodrums11@gmail.com>
2018-04-21 15:19:32 -0300

    update news operations

--- fila/Fila.h
+++ fila/Fila.h
@@ -2,13 +2,14 @@ typedef struct fila Fila;
 
 Fila * criar();
 void destruir(Fila * f);
-
 int desenfileirar(Fila * f); 
 void enfileirar(Fila * f, int v);
 int tamanho(Fila * f);
 int primeiro(Fila * f);
 
 /* novas funcoes */
-int esta_vazia(Fila f);
-void limpar(Fila f);
-
+void imprimir(Fila * f);
+int ultimo(Fila * f);
+int elemento_meio(Fila * f);
+int esta_vazia(Fila * f);
+void limpar(Fila * f);
